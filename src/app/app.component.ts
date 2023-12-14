5c624f965291886d17d308a327fc43412bce975f
Juan Lagunas <dev.jlagunas@gmail.com>
2023-12-14 14:38:17 +0900

    Initial configuration and components creation complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { ArticleComponent } from './article/article.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ArticleComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'article';
-}
+export class AppComponent {}
